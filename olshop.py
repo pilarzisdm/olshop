b8d0f5dfaaec5cec8ba5ff615f7b60bc484df561
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-13 07:56:35 +0000

    1

--- olshop.py
+++ olshop.py
@@ -10,12 +10,12 @@ def get_shops_in_location(location):
         shops = soup.find_all('div', class_='shop-item')  # Adjust the class based on the website structure
 
         for shop in shops:
-            shop_name = shop.find('div', class_='shop-item__title').text.strip()
+            shop_name = shop.find('div', class_='shop-item__title').text.strip()  # Update class name
             products = shop.find_all('div', class_='product-item')  # Adjust the class based on the website structure
 
             print(f'Shop: {shop_name}')
             for product in products:
-                product_name = product.find('div', class_='product-item__title').text.strip()
+                product_name = product.find('div', class_='product-item__title').text.strip()  # Update class name
                 print(f'  Product: {product_name}')
 
     else:
