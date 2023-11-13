fb75a68159d27d8abbcabdcf8ee7272906c600b7
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-13 09:47:23 +0000

    1

--- olshop.py
+++ olshop.py
@@ -1,26 +1,71 @@
-import requests
+# shopee.py
+from selenium import webdriver
 from bs4 import BeautifulSoup
+import re
+import csv
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+import time
 
-def get_shops_in_location(location):
-    url = f'https://shopee.co.id/search?keyword={location}'
-    response = requests.get(url)
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('log-level=2')
+driver = webdriver.Chrome(options=chrome_options)
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        shops = soup.find_all('div', class_='shop-item')  # Adjust the class based on the website structure
+katakunci = input('Masukkan kata kunci : ')
 
-        for shop in shops:
-            shop_name = shop.find('div', class_='shop-item__title').text.strip()  # Update class name
-            products = shop.find_all('div', class_='product-item')  # Adjust the class based on the website structure
+def search(katakunci):
+    links = []
+    print('mencari semua product dengan kata kunci ' + katakunci)
+    url = 'https://shopee.co.id/search?keyword=' + katakunci
+    try:
+        driver.get(url)
+        time.sleep(5)
+        driver.execute_script('window.scrollTo(0, 1500);')
+        time.sleep(5)
+        driver.execute_script('window.scrollTo(0, 2500);')
+        time.sleep(5)
+        soup_a = BeautifulSoup(driver.page_source, 'html.parser')
+        products = soup_a.find('div', class_='row shopee-search-item-result__items')
+        for link in products.find_all('a'):
+            links.append(link.get('href'))
+            print(link.get('href'))
+    except TimeoutException:
+        print('failed to get links with query ' + line)
+    return links
 
-            print(f'Shop: {shop_name}')
-            for product in products:
-                product_name = product.find('div', class_='product-item__title').text.strip()  # Update class name
-                print(f'  Product: {product_name}')
+def get_product(produt_url):
+    try:
+        url = 'https://shopee.co.id' + produt_url
+        driver.get(url)
+        time.sleep(3)
+        driver.execute_script('window.scrollTo(0, 1500);')
+        time.sleep(3)
+        WebDriverWait(driver, timeout).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, 'OSgLcw')))
+        soup_b = BeautifulSoup(driver.page_source, 'html.parser')
+        title = soup_b.find('span', class_='OSgLcw').text
+        price = soup_b.find('div', class_='_3n5NQx').text
+        try:
+            image = soup_b.find('div', class_='_2JMB9h _3XaILN')['style']
+            imgurl = re.findall('url\((.*?)\)', image)
+        except:
+            imgurl = 'none'
+        desc = soup_b.find('div', class_='_2u0jt9').text
+        print('Scraping ' + title)
+        with open('result.csv','a', encoding='utf-8',newline='') as f:
+            writer=csv.writer(f)
+            writer.writerow([title,price,url,desc,imgurl])
 
-    else:
-        print(f'Error {response.status_code}: Unable to fetch data.')
+    except TimeoutException:
+        print('cant open the link')
 
-if __name__ == '__main__':
-    location = input('Enter location: ')
-    get_shops_in_location(location)
+products_urls = search(katakunci)
+
+for product_url in products_urls:
+    get_product(product_url)
+
+driver.quit()
