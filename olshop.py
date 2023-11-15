885a492836476e7f8c84512dd4fef235c4792eb2
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-15 10:13:05 +0000

    1

--- olshop.py
+++ olshop.py
@@ -1,4 +1,6 @@
-# shopee.pysepatu
+# streamlit_shopee.py
+
+import streamlit as st
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
@@ -10,18 +12,12 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 import time
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--log-level=2')
-driver = webdriver.Chrome(options=chrome_options)
-
-katakunci = input('Masukkan kata kunci : ')
-
+# Function to search for products
 def search(katakunci):
     links = []
-    print('mencari semua product dengan kata kunci ' + katakunci)
+    st.write(f'Mencari semua produk dengan kata kunci: {katakunci}')
     url = 'https://shopee.co.id/search?keyword=' + katakunci
-    
+
     try:
         driver.get(url)
         time.sleep(5)
@@ -33,14 +29,15 @@ def search(katakunci):
         products = soup_a.find('div', class_='row shopee-search-item-result__items')
         for link in products.find_all('a'):
             links.append(link.get('href'))
-            print(link.get('href'))
+            st.write(link.get('href'))
     except TimeoutException:
-        print('failed to get links with query ' + line)
+        st.write(f'Failed to get links with query {katakunci}')
     return links
 
-def get_product(produt_url):
+# Function to get product details
+def get_product(product_url):
     try:
-        url = 'https://shopee.co.id' + produt_url
+        url = 'https://shopee.co.id' + product_url
         driver.get(url)
         time.sleep(3)
         driver.execute_script('window.scrollTo(0, 1500);')
@@ -56,21 +53,37 @@ def get_product(produt_url):
         except:
             imgurl = 'none'
         desc = soup_b.find('div', class_='_2u0jt9').text
-        print('Scraping ' + title)
-        with open('result.csv','a', encoding='utf-8',newline='') as f:
-            writer=csv.writer(f)
-            writer.writerow([title,price,url,desc,imgurl])
+        st.write(f'Scraping {title}')
+        with open('result.csv', 'a', encoding='utf-8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([title, price, url, desc, imgurl])
 
     except TimeoutException:
-        print('cant open the link')
+        st.write('Cannot open the link')
 
-products_urls = search(katakunci)
+# Streamlit app
+st.title("Shopee Scraper")
 
-for product_url in products_urls:
-    get_product(product_url)
+katakunci = st.text_input('Masukkan kata kunci:')
 
+# Create a button to start the scraping process
+if st.button("Cari Produk"):
+    st.write("Sedang mencari produk...")
+    products_urls = search(katakunci)
+
+    for product_url in products_urls:
+        get_product(product_url)
+
+    st.write("Proses selesai.")
+
+# WebDriver initialization
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--log-level=2')
+driver = webdriver.Chrome(options=chrome_options)
+
+# Exception handling for quitting WebDriver
 try:
     driver.quit()
 except Exception as e:
-    print(f"Error while quitting WebDriver: {str(e)}")
-
+    st.write(f"Error while quitting WebDriver: {str(e)}")
