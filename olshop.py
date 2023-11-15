659ff4aa116dbff5c5b43a7ba899321389f0bbd7
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-15 09:50:07 +0000

    1

--- olshop.py
+++ olshop.py
@@ -21,6 +21,7 @@ def search(katakunci):
     links = []
     print('mencari semua product dengan kata kunci ' + katakunci)
     url = 'https://shopee.co.id/search?keyword=' + katakunci
+    
     try:
         driver.get(url)
         time.sleep(5)
@@ -29,7 +30,7 @@ def search(katakunci):
         driver.execute_script('window.scrollTo(0, 2500);')
         time.sleep(5)
         soup_a = BeautifulSoup(driver.page_source, 'html.parser')
-        products = soup_a.find('div', class_='shopee-search-item-result')
+        products = soup_a.find('div', class_='row shopee-search-item-result__items')
         for link in products.find_all('a'):
             links.append(link.get('href'))
             print(link.get('href'))
